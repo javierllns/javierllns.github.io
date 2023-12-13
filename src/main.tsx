b3c5a06b032a9ee3946af14c09327a1c5c8c6a7f
javierllns <llanosmj1@gmail.com>
2023-12-12 23:10:15 -0500

    installed tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <h1 className='bg-slate-800 p-5'>HOLA!</h1>
+  </React.StrictMode>
 )
