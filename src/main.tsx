64961d801329a47d2b4a5b1e8b8721ed74c9aeb8
javierllns <llanosmj1@gmail.com>
2023-12-13 13:23:22 -0500

    added clsx and prettier plugin for tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { clsx } from 'clsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1 className='bg-slate-800 p-5'>HOLA!</h1>
+    <div className='h-screen bg-stone-800 p-4'>
+      <h1
+        className={clsx(
+          'bg-orange-700',
+          true && 'm-5 flex bg-slate-800 p-8 text-zinc-200',
+          true && 'bg-red-600 p-9'
+        )}
+      >
+        HOLA!
+      </h1>
+    </div>
   </React.StrictMode>
 )
