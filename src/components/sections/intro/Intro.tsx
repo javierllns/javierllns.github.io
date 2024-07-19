e602c0f25644a3908001c71508f2360e27152657
javierllns <llanosmj1@gmail.com>
2024-07-19 04:06:06 -0500

    change parallax library

--- src/components/sections/intro/Intro.tsx
+++ src/components/sections/intro/Intro.tsx
@@ -1,44 +1,36 @@
-import { cn } from '@/components/lib/utils'
-import React, { useEffect } from 'react'
-import Rellax from 'rellax'
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax'
 
-const BlurredItem: React.FC<{ children: React.ReactNode; className?: string; blurClassName?: string }> = ({
-  children,
-  className,
-  blurClassName
-}) => {
-  return (
-    <div className={cn(className ? className : '', 'size-fit p-6', 'flex items-center justify-center')}>
-      {children}
-      <div className={cn(blurClassName ? blurClassName : '', 'absolute top-0 left-0', 'size-full')} />
-    </div>
-  )
-}
+// const BlurredItem: React.FC<{ children: React.ReactNode; className?: string; blurClassName?: string }> = ({
+//   children,
+//   className,
+//   blurClassName
+// }) => {
+//   return (
+//     <div className={cn(className ? className : '', 'size-fit p-6', 'flex items-center justify-center')}>
+//       {children}
+//       <div className={cn(blurClassName ? blurClassName : '', 'absolute top-0 left-0', 'size-full')} />
+//     </div>
+//   )
+// }
 
 export default function Intro() {
-  useEffect(() => {
-    var rellax = new Rellax('.parallax')
-  }, [])
-
   return (
-    <div className={'overflow-hidden h-[500px] rounded-lg relative'}>
-      <div className='parallax absolute top-60 left-[20%]' data-rellax-speed='-3'>
-        <BlurredItem className='!p-8' blurClassName='backdrop-blur-md'>
-          <div id='box' className={cn('size-[70px]', 'bg-red-200')} />
-        </BlurredItem>
-      </div>
+    <div className='relative'>
+      <ParallaxProvider>
+        <div className={'relative  h-[500px] overflow-hidden'}>
+          <Parallax speed={-10} className='absolute top-60 left-[20%]'>
+            <div className='size-[70px] bg-red-200' />
+          </Parallax>
 
-      <div className='parallax absolute top-20 left-[50%] !rotate-45' data-rellax-speed='7'>
-        <BlurredItem>
-          <div id='box' className={cn('size-[100px]', 'bg-green-200')} />
-        </BlurredItem>
-      </div>
+          <Parallax speed={21} className='absolute top-32 left-[50%]'>
+            <div className='size-[70px] bg-green-200' />
+          </Parallax>
 
-      <div className='parallax absolute top-72 right-[20%]' data-rellax-speed='-5'>
-        <BlurredItem blurClassName='backdrop-blur-sm'>
-          <div className={cn('size-[90px]', 'bg-blue-200')} />
-        </BlurredItem>
-      </div>
+          <Parallax speed={-15} className='absolute top-72 right-[20%]'>
+            <div className='size-[70px] bg-blue-200' />
+          </Parallax>
+        </div>
+      </ParallaxProvider>
     </div>
   )
 }
