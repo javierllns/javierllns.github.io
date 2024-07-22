d7256232dc95b8fe824b8cbbdfea14bc0cb35b80
javierllns <llanosmj1@gmail.com>
2024-07-22 18:49:21 -0500

    url for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://javierllns.github.io/',
   integrations: [tailwind({ applyBaseStyles: false }), react()]
 })
