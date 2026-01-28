ff783af5e100319b426c22baecec1282d9c8e099
javierllns <82537472+javierllns@users.noreply.github.com>
2026-01-27 19:33:15 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://javierllns.github.io/',
+  site: 'https://portfolio.llns.dev/',
   integrations: [tailwind({ applyBaseStyles: false }), react()]
 })
